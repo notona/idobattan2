711e6a0d631c25210006ad84ac1218cd6bc95586
notona <notona@gmail.com>
2015-10-27 22:30:47 +0900

    fix: Can't use copy paste on the Mac

--- main.js
+++ main.js
@@ -44,4 +44,45 @@ app.on('ready', function() {
   });
     
   mainWindow.loadUrl('file://' + __dirname + '/index.html'); 
+
+  setApplicationMenu();
 });
+
+function setApplicationMenu (){
+  var Menu = require('menu');
+  var menu = Menu.buildFromTemplate([
+    {
+      label: "Application",
+      submenu: [
+        {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+        {type: "separator"},
+        {label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "Command+X", selector: "cut:" },
+        { label: "Copy", accelerator: "Command+C", selector: "copy:" },
+        { label: "Paste", accelerator: "Command+V", selector: "paste:" }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+}
